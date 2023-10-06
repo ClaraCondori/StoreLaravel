47244a8d59f178b0fe850c17a4c25e2292b27ea8
Clara Condori <condorihclara62@gmsil.com>
2023-10-06 02:06:56 -0300

    creando carpeta productos y añadiendo cr a ambas carpetas

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,7 +31,7 @@
         <div class="row">
             <div class="col-md-2"></div>
             <div class="col-md-8">
-                hola tablas
+                @yield('content')
             </div>
             <div class="col-md-2"></div>
         </div>
